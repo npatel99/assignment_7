727e7391d9faba91fc9b2757e898f09e0b2731a7
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-07-17 09:36:46 +0530

    Fibonnaci series

---
+++ prog_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/*Write a program to find the Nth term of the Fibonnaci series.
+*Fibonnaci series - 0 1 1 2 3 5 8 13 21 34 55 89 ........
+*Fn = Fn-1 + Fn-2; F0 = 0, F1 = 1
+*/
+int main()
+{
+    int f0 = 0, f1 = 1, f2, n, i;
+    printf("Enter the n element: ");
+    scanf("%d", &n);
+    printf("Fibonnaci series: %d %d ", f0, f1);
+    for(i=2; i<n ; i++)
+    {
+        f2 = f0+f1;
+        printf("%d ", f2);
+        f0 = f1;
+        f1 = f2;
+    }
+    printf("\n");
+    return 0;
+}
