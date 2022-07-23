cbd7610ef9d43a39fe3b849af993e04a6954d28c
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-07-23 18:11:36 +0530

    first nth terms of Fabonacci series

--- prog_2.c
+++ prog_2.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
 /*Write a program to print first N terms of Fibonacci series
-*Fibonnaci series - 0 1 1 2 3 5 8 13 21 34 55 89 ........
-*Fn = Fn-1 + Fn-2; F0 = 0, F1 = 1
+*Fibonnaci series - 1 1 2 3 5 8 13 21 34 55 89 ........
+*Fn = Fn-1 + Fn-2; next = curr + prev;
 */
 int main()
 {
-    int f0 = 0, f1 = 1, f2, n, i;
-    printf("Enter the n element: ");
+    int prev = 0, curr = 1, next, n, i;
+    printf("Enter the nth term : ");
     scanf("%d", &n);
-    printf("Fibonnaci series: %d %d ", f0, f1);
-    for(i=2; i<n ; i++)
+    printf("Fabonacci series is: %d ", curr);
+    
+    for(i=1; i<n ; i++)
     {
-        f2 = f0+f1;
-        printf("%d ", f2);
-        f0 = f1;
-        f1 = f2;
+        next = prev + curr;
+        printf("%d ", next);
+        prev = curr;
+        curr = next;
     }
     printf("\n");
     return 0;
